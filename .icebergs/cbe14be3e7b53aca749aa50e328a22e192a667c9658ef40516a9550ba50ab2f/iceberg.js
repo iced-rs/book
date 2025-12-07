7439e98faf4862a9e8392426a629ddaa6732aa6f
hecrj <hecrj@users.noreply.github.com>
2025-12-07 03:04:09 +0000

    deploy: c51646d7b2e779e26010501711ef9ea1345a93e3

--- .icebergs/cbe14be3e7b53aca749aa50e328a22e192a667c9658ef40516a9550ba50ab2f/iceberg.js
+++ .icebergs/cbe14be3e7b53aca749aa50e328a22e192a667c9658ef40516a9550ba50ab2f/iceberg.js
@@ -294,12 +294,12 @@ function wasm_bindgen__convert__closures_____invoke__h3d7d68b98672e57f(arg0, arg
     wasm.wasm_bindgen__convert__closures_____invoke__h3d7d68b98672e57f(arg0, arg1, arg2);
 }
 
-function wasm_bindgen__convert__closures_____invoke__h1286be8af5e963f5(arg0, arg1) {
-    wasm.wasm_bindgen__convert__closures_____invoke__h1286be8af5e963f5(arg0, arg1);
-}
-
 function wasm_bindgen__convert__closures_____invoke__hb8b6941fceac7069(arg0, arg1, arg2) {
     wasm.wasm_bindgen__convert__closures_____invoke__hb8b6941fceac7069(arg0, arg1, arg2);
+}
+
+function wasm_bindgen__convert__closures_____invoke__h1286be8af5e963f5(arg0, arg1) {
+    wasm.wasm_bindgen__convert__closures_____invoke__h1286be8af5e963f5(arg0, arg1);
 }
 
 const __wbindgen_enum_GpuAddressMode = ["clamp-to-edge", "repeat", "mirror-repeat"];
